72031a7b2a73fde8a4bf701f67d477762392395a
Matt Fryer <MattFryer@users.noreply.github.com>
2021-03-18 18:15:04 +0000

    Initial create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+	/usr/bin/java -server -Xms128m -Xmx1024m -XX:MaxHeapFreeRatio=60 -XX:MinHeapFreeRatio=30 -XX:+HeapDumpOnOutOfMemoryError -cp /opt/tplink/EAPController/lib/*: com.tplink.omada.start.OmadaLinuxMain
