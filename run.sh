9049213d3cdb792839d8c5f5897fbce36d5d7be1
Matt Fryer <MattFryer@users.noreply.github.com>
2021-03-19 09:49:11 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,1 +0,0 @@
-	/usr/bin/java -server -Xms128m -Xmx1024m -XX:MaxHeapFreeRatio=60 -XX:MinHeapFreeRatio=30 -XX:+HeapDumpOnOutOfMemoryError -cp /opt/tplink/EAPController/lib/*: com.tplink.omada.start.OmadaLinuxMain
